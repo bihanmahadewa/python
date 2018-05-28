c3e5e4ce2143b319a6978e25475070438fcc256e
Bihan Mahadewa <bihanmahadewa@Bihans-MacBook-Pro.local>
2018-05-28 15:35:37 +0530

    The Basics of pthon 3

---
+++ Basics of Python/basics.py
@@ -0,0 +1,45 @@
+__author__ = "Bihan"
+
+#print statements in Python
+print("Hello Python")
+
+print(1+2)
+print(7*6)
+print
+
+
+print("The End")
+
+print('we can also include "quotes" in Strigs')
+
+print('hello' + ' python')
+
+
+#A comment in Python
+
+#variables in Python
+
+greeting = "hello"
+name = " Bihan"
+age = 10
+
+
+print(age)
+print
+print(greeting + name)
+
+print (6 + 7)
+
+#split string
+
+splitStr = "Hi Bihan \n How is life"
+
+tabs = "\t\t2\t3\t4\t5"
+print(splitStr)
+
+
+print('the pet owner told "No,No, \'e\'s sh.... he\s resting"')
+
+anotherSplitstring = """"dsadsdsadsasda"""""
+
+print(anotherSplitstring)
---
+++ Basics of Python/variables.py
@@ -0,0 +1,16 @@
+age = 24
+greet = "hi"
+
+print(greet + age)
+
+
+# Integer
+a = 12
+b = 3
+
+print (a / b)
+print (a // b)
+
+#Float
+floatNum = 2.02
+
